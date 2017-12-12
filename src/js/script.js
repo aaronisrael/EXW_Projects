75f332d57df514a00ce663241873161f68b7b858
Aaron Israel <aaron.israel1103@gmail.com>
2017-12-12 10:29:33 +0100

    🔥 Remove testing

--- src/js/script.js
+++ src/js/script.js
@@ -44,10 +44,6 @@ const sizeRandomness = 40000;
 const parts = [];
 const dirs = [];
 
-const stop = false;
-let frameCount = 0;
-let fps, fpsInterval, startTime, now, then, elapsed;
-
 // ------------------------------------- //
 // ------- GAME FUNCTIONS -------------- //
 // ------------------------------------- //
@@ -65,7 +61,7 @@ const setup = () => {
   createTable();
   lights();
 
-  startAnimating(60);
+  draw();
 
   const socket = io.connect(`0.0.0.0:3000`);
   socket.on(`connect`, () => {
@@ -237,57 +233,6 @@ const updateStars = () => {
     }
   }
 
-};
-
-<<<<<<< HEAD
-const draw = () => {
-  // draw THREE.JS scene
-  renderer.render(scene, camera);
-  //requestAnimationFrame(draw);
-  updateStars();
-  ballPhysics();
-  paddlePhysics();
-  playerPaddleMovement();
-  opponentPaddleMovement();
-};
-=======
->>>>>>> master
-
-const startAnimating = fps => {
-  fpsInterval = 1000 / fps;
-  then = Date.now();
-  startTime = then;
-  animate();
-};
-
-const animate = () => {
-  if (stop) {
-    return;
-  }
-
-    // request another frame
-  requestAnimationFrame(animate);
-    // calc elapsed time since last loop
-
-  now = Date.now();
-  elapsed = now - then;
-
-    // if enough time has elapsed, draw the next frame
-
-  if (elapsed > fpsInterval) {
-
-        // Get ready for next frame by setting then=now, but...
-        // Also, adjust for fpsInterval not being multiple of 16.67
-    then = now - (elapsed % fpsInterval);
-
-    draw();
-
-
-        // TESTING...Report #seconds since start and achieved fps.
-    const sinceStart = now - startTime;
-    const currentFps = Math.round(1000 / (sinceStart / ++ frameCount) * 100) / 100;
-    //console.log(currentFps);
-  }
 };
 
 const ballPhysics = () => {
