9087c0948f1ca049727b45aae725256a466d0457
Aaron Israel <aaron.israel1103@gmail.com>
2017-12-18 23:31:34 +0100

    add path

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const {ifProduction} = getIfUtils(process.env.NODE_ENV);
 const extractCSS = new ExtractTextWebpackPlugin(`css/style.css`);
 
 // change for production build on different server path
-const publicPath = `/`;
+const publicPath = `http://nameless-citadel-81819.herokuapp.com/`;
 
 const copy = new CopyWebpackPlugin([{
   from: `./src/assets`,
